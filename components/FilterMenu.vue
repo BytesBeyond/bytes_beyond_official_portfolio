19da66286600e80072ea0232eda15a0befe7798d
HridaySarma <hridaysarma8@gmail.com>
2022-10-15 21:18:46 +0530

    Initial commit

---
+++ components/FilterMenu.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "FilterMenu.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <FilterMenu/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import FilterMenu from "~/components/FilterMenu.vue";
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {FilterMenu}
+})
+</script>
